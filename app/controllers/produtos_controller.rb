f562f3ce843f22fad32c72b0af257eab6a31aab3
Luana Aniceto <luanaaniceto@luana.aniceto>
2020-07-21 11:16:16 -0300

    ajustes clean code

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -29,8 +29,7 @@ class ProdutosController < ApplicationController
 
     respond_to do |format|
       if @produto.save
-        report = ReportController.new
-        report.csv
+        create_csv
         format.html { redirect_to @produto, notice: 'Produto criado com sucesso.' }
         format.json { render :show, status: :created, location: @produto }
       else
@@ -45,8 +44,7 @@ class ProdutosController < ApplicationController
   def update
     respond_to do |format|
       if @produto.update(produto_params)
-        report = ReportController.new
-        report.csv
+        create_csv
         format.html { redirect_to @produto, notice: 'Produto atualizado com sucesso.' }
         format.json { render :show, status: :ok, location: @produto }
       else
@@ -60,8 +58,7 @@ class ProdutosController < ApplicationController
   # DELETE /produtos/1.json
   def destroy
     @produto.destroy
-    report = ReportController.new
-    report.csv
+    create_csv
     respond_to do |format|
       format.html { redirect_to produtos_url, notice: 'Produto deletado com sucesso.' }
       format.json { head :no_content }
@@ -77,5 +74,10 @@ class ProdutosController < ApplicationController
     # Only allow a list of trusted parameters through.
     def produto_params
       params.require(:produto).permit(:sku, :nome, :descricao, :quantidade, :preco, :codigo_barra)
+    end
+
+    def create_csv
+      report = ReportController.new
+      report.csv
     end
 end
